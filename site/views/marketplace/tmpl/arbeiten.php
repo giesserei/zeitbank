12906ab98981d94d2bc7fc4c8a54fb03c6fe9e0d
Steffen Förster <github96758@4foerster.ch>
2016-09-04 21:45:57 +0200

    Bug-Fix

--- site/views/marketplace/tmpl/arbeiten.php
+++ site/views/marketplace/tmpl/arbeiten.php
@@ -2,7 +2,7 @@
 defined('_JEXEC') or die;
 
 // AJAX-Tools laden
-JHTML::_('behavior.mootools');
+JHTML::_('behavior.framework');
 JHTML::_('behavior.modal');
 
 JLoader::register('ZeitbankFrontendHelper', JPATH_COMPONENT . '/helpers/zeitbank_frontend.php');
